500e6a3d2fa2c1b7d2954f35af2e8b4d1d423d5c
aayushmadan <aayush.madan4611@gmail.com>
2026-05-18 13:04:13 +0530

    Fix in final pipeline

--- app.py
+++ app.py
@@ -84,5 +84,10 @@ def chat():
         return jsonify({"error": "An error occurred. Please try again."}), 500
 
 
+@app.route('/health')
+def health():
+    return 'Server is up and running'
+
+
 # if __name__ == "__main__":
 #     app.run(host="0.0.0.0", port=5000, debug=False)
